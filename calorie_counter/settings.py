31c42bd4607bf18789e26673002f8c98ac584279
ShristiGup <guptashristi25@gmail.com>
2020-12-21 16:30:16 +0530

    Update settings.py

--- calorie_counter/settings.py
+++ calorie_counter/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#@15sq#9q_cn(a++x!bo4%=b=dvccvzov213om_^e$@457a#^%'
+SECRET_KEY = str(os.environ.get('calorie_key'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
